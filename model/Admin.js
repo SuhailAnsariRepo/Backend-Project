96a17a48258811f0cc84b519b86da30ed0035c49
Suhail Ansari <suhafea@gmail.com>
2023-06-06 23:17:50 +0530

    Counter

--- model/Admin.js
+++ model/Admin.js
@@ -5,7 +5,8 @@ const CounterSchema = new mongoose.Schema({
     sequence_value: { type: Number, default: 1 }
   });
 
-  
+const Counter = mongoose.model('Counter', CounterSchema);
+
 const adminSchema = new mongoose.Schema({
     admin_id : {
         type:Number,
