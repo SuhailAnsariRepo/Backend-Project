70dc362115230270811ebdf5b692ba8e6e8b46a6
Suhail Ansari <suhafea@gmail.com>
2023-06-06 23:25:46 +0530

    made some changes

--- model/Admin.js
+++ model/Admin.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const CounterSchema = new mongoose.Schema({
-    _id: { type: String, required: true },
-    sequence_value: { type: Number, default: 1 }
-  });
+// const CounterSchema = new mongoose.Schema({
+//     _id: { type: String, required: true },
+//     sequence_value: { type: Number, default: 1 }
+//   });
 
-const Counter = mongoose.model('Counter', CounterSchema);
+// const Counter = mongoose.model('Counter', CounterSchema);
 
 const adminSchema = new mongoose.Schema({
     admin_id : {
@@ -22,21 +22,21 @@ const adminSchema = new mongoose.Schema({
     }
 });
 
-adminSchema.pre('save', function (next) {
-    const doc = this;
-    Counter.findByIdAndUpdate(
-      { _id: 'adminId' },
-      { $inc: { sequence_value: 1 } },
-      { new: true, upsert: true }
-    )
-      .then((counter) => {
-        doc.admin_id = counter.sequence_value;
-        next();
-      })
-      .catch((error) => {
-        next(error);
-      });
-  });
+// adminSchema.pre('save', function (next) {
+//     const doc = this;
+//     Counter.findByIdAndUpdate(
+//       { _id: 'adminId' },
+//       { $inc: { sequence_value: 1 } },
+//       { new: true, upsert: true }
+//     )
+//       .then((counter) => {
+//         doc.admin_id = counter.sequence_value;
+//         next();
+//       })
+//       .catch((error) => {
+//         next(error);
+//       });
+//   });
   
 
 const Admin = mongoose.model("Admin", adminSchema);
--- model/User.js
+++ model/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const CounterSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  sequence_value: { type: Number, default: 1 }
-});
+// const CounterSchema = new mongoose.Schema({
+//   _id: { type: String, required: true },
+//   sequence_value: { type: Number, default: 1 }
+// });
 
-const Counter = mongoose.model('Counter', CounterSchema);
+// const Counter = mongoose.model('Counter', CounterSchema);
 
 const UserSchema = new mongoose.Schema({
   user_id: { type: Number, unique: true },
@@ -15,21 +15,21 @@ const UserSchema = new mongoose.Schema({
   kyc: { type: String },
 });
 
-UserSchema.pre('save', function (next) {
-  const doc = this;
-  Counter.findByIdAndUpdate(
-    { _id: 'userId' },
-    { $inc: { sequence_value: 1 } },
-    { new: true, upsert: true }
-  )
-    .then((counter) => {
-      doc.User_id = counter.sequence_value;
-      next();
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
+// UserSchema.pre('save', function (next) {
+//   const doc = this;
+//   Counter.findByIdAndUpdate(
+//     { _id: 'UserId' },
+//     { $inc: { sequence_value: 1 } },
+//     { new: true, upsert: true }
+//   )
+//     .then((counter) => {
+//       doc.User_id = counter.sequence_value;
+//       next();
+//     })
+//     .catch((error) => {
+//       next(error);
+//     });
+// });
 
 const User = mongoose.model('User', UserSchema);
 
