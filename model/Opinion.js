4654e7cf83a189077d9868783864624659df97e1
Suhail Ansari <56288671+SuhailAnsariRepo@users.noreply.github.com>
2023-06-06 15:01:04 +0530

    Update Opinion.js

--- model/Opinion.js
+++ model/Opinion.js
@@ -1,30 +1,37 @@
-const mongoose = require("mongoose");
-// const autoIncrement = require('mongoose-sequence')(mongoose);
+const mongoose = require('mongoose');
 
-const OpinionSchema = new mongoose.Schema({
-    opinion_id: {
-        type: Number,
-        required: true,
-    },
-    title: {
-        type: String
-    },
-    description: {
-        type: String
-    },
-    start_date: {
-        type: Date
-    },
-    end_date: {
-        type: Date
-    },
-    options: {
-        type: String
-    }
+const CounterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  sequence_value: { type: Number, default: 1 }
 });
 
-// OpinionSchema.plugin(autoIncrement);
+const Counter = mongoose.model('Counter', CounterSchema);
 
+const OpinionSchema = new mongoose.Schema({
+  opinion_id: { type: Number, unique: true },
+  title: { type: String },
+  description: { type: String },
+  start_date: { type: Date },
+  end_date: { type: Date },
+  options: { type: String }
+});
 
-const Opinion = mongoose.model("Opinion", OpinionSchema);
+OpinionSchema.pre('save', function (next) {
+  const doc = this;
+  Counter.findByIdAndUpdate(
+    { _id: 'opinionId' },
+    { $inc: { sequence_value: 1 } },
+    { new: true, upsert: true }
+  )
+    .then((counter) => {
+      doc.opinion_id = counter.sequence_value;
+      next();
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+const Opinion = mongoose.model('Opinion', OpinionSchema);
+
 module.exports = Opinion;
