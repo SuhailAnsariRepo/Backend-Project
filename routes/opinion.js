b7ab05f2e90cc9121aec9e99449d40a3e25bb29f
Suhail Ansari <56288671+SuhailAnsariRepo@users.noreply.github.com>
2023-06-07 20:38:25 +0530

    Update opinion.js

--- routes/opinion.js
+++ routes/opinion.js
@@ -4,7 +4,7 @@ const opinionController = require('../controllers/opinionController')
 router.post("/", opinionController.add_opinion);
 router.get("/", opinionController.all_opinions);
 router.get("/:opinion_id", opinionController.get_opinion);
-router.put("update/:opinion_id", opinionController.update_opinion);
+router.put("/update/:opinion_id", opinionController.update_opinion);
 router.get("/getStatus/:status", opinionController.get_status);
 
 
