b52a0ca7373f0da5035cd208c9ae909ba400c7c4
Suhail Ansari <suhail.ansari@wiom.in>
2023-06-10 11:09:34 +0530

    made route for partner

---
+++ routes/partner.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const partnerController = require('../controllers/partnerController')
+
+router.put("update/:mobile", partnerController.update_partner);
+router.get("/", partnerController.all_partners);
+router.get("/:mobile", partnerController.get_partner);
+router.post("/", partnerController.add_partner);
+router.post("/login", partnerController.login_partner);
+router.delete("/:mobile", partnerController.delete_partner);
+
+
+module.exports = router;
