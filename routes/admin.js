b4c13268ea18312e78e25860f41401c381f39878
Suhail Ansari <suhail.ansari@wiom.in>
2023-06-14 17:31:47 +0530

    adding mobile as primary key

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const adminController = require('../controllers/adminController')
 
-router.put("/update/:email", adminController.update_admin);
+router.put("/update/:mobile", adminController.update_admin);
 router.get("/:role", adminController.all_admins);
-router.get("/:email", adminController.get_admin);
+router.get("/:mobile", adminController.get_admin);
 router.post("/", adminController.add_admin);
-router.delete("/:email", adminController.delete_admin);
+router.delete("/:mobile", adminController.delete_admin);
 router.post("/signup", adminController.sign_up);
 router.post("/login", adminController.login);
 
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const userController = require('../controllers/userController')
 
-router.put("/update/:user_id", userController.update_user);
+router.put("/update/:mobile", userController.update_user);
 router.get("/", userController.all_users);
 router.get("/:mobile", userController.get_user);
 router.post("/", userController.add_user);
-router.delete("/:user_id", userController.delete_user);
+router.delete("/:mobile", userController.delete_user);
 router.post("/signup", userController.sign_up);
 router.post("/login", userController.login);
 
