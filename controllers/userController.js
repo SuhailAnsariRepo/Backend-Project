1d29497d2e7bbdfcb77281a92e838087466f6b72
Suhail Ansari <suhafea@gmail.com>
2023-06-26 20:45:13 +0530

    removed fast2sms

--- controllers/userController.js
+++ controllers/userController.js
@@ -205,31 +205,31 @@ const generateOTP = async (req, res, next) => {
       return res.status(409).json({ message: "User not found. Please Sign up" });
     }
 
-    let random = Math.floor(Math.random() * 900000) + 10000;
+    let random = Math.floor(Math.random() * 900000) + 100000;
     existingUser.otp = random;
     await existingUser.save();
 
 
-    const fast2sms = require('fast-two-sms')
+    // const fast2sms = require('fast-two-sms')
 
-    var options = {authorization : "lwbzF3rIk5WT0E8S2JZf6RmBDnvYxGuVhO4XA1KeNg7QLiMspUyM4R0EbV9jBteOlhC3i6IfHdgnJcYx" , message : 'Hey You there' ,  numbers : [mobile]} 
-    const res2 = await fast2sms.sendMessage(options) //Asynchronous Function.
+    // var options = {authorization : "lwbzF3rIk5WT0E8S2JZf6RmBDnvYxGuVhO4XA1KeNg7QLiMspUyM4R0EbV9jBteOlhC3i6IfHdgnJcYx" , message : 'Hey You there' ,  numbers : [mobile]} 
+    // const res2 = await fast2sms.sendMessage(options) //Asynchronous Function.
 
-    console.log(res2);
+    // console.log(res2);
 
-    // accountSid = 'ACc5358182ec884510e49dfac5daf8c6cb';
-    // authToken = '6f111c8270baef0b07541dd837b2e37c';
+    accountSid = 'ACc5358182ec884510e49dfac5daf8c6cb';
+    authToken = '6f111c8270baef0b07541dd837b2e37c';
 
-    // const client = require('twilio')(accountSid, authToken);
+    const client = require('twilio')(accountSid, authToken);
 
-    // client.messages
-    //   .create({
-    //     body: `Your OTP for Earn-X is ${random}.`,
-    //     from: '+15418738806',
-    //     to: '+91' + mobile
-    //   })
-    //   .then(message => console.log(message.sid))
-    //   .catch(error => console.error(error));
+    client.messages
+      .create({
+        body: `Your OTP for Earn-X is ${random}.`,
+        from: '+15418738806',
+        to: '+91' + mobile
+      })
+      .then(message => console.log(message.sid))
+      .catch(error => console.error(error));
 
     res.status(200).json({ otp: random });
     next();
